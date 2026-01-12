124192a061f413dc2b30f8b431f1cd08c794b3e2
zenquessence <kkazuha6@gmail.com>
2026-01-12 16:38:31 +0700

    Delete peminjaman_kamera directory

--- peminjaman_kamera/config/cek_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['login'])) {
-    header("Location: ../auth/login.php");
-    exit;
-}
--- peminjaman_kamera/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Sistem Peminjaman Kamera | Developed by <b>Zain Adnan Hanif</b> - A12.2024.07263
-    </p>
-</footer>
